28047d90b589c81edb0800cc676abc77e387d4dc
P10 <p10@P10s-Mac-mini.local>
2023-10-13 11:55:48 +0530

    Added header and footer to the contacts

--- multipleList/multipleList/TableViewController.swift
+++ multipleList/multipleList/TableViewController.swift
@@ -21,7 +21,8 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         "John" : "123",
         "David" : "456",
         "Elsa" : "789",
-        "Maryse": "10101010"
+        "Maryse": "10101010",
+        "A": "156"
     ]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -47,6 +48,46 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         navigationController?.pushViewController(destinationVC, animated: true)
     }
     
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            // Create and configure a custom header view
+            let headerView = UIView()
+            headerView.backgroundColor = UIColor.white
 
+            // Create a label for the header text
+            let headerLabel = UILabel()
+            headerLabel.text = "Contacts"
+            headerLabel.textColor = UIColor.black
+            headerLabel.frame = CGRect(x: 15, y: 5, width: 200, height: 20)
+            headerLabel.font = UIFont.boldSystemFont(ofSize: 24)
+            
+            headerView.addSubview(headerLabel)
+            
+            return headerView
+        }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        // Create and configure a custom footer view
+        let footerView = UIView()
+
+        // Create a label for the footer text
+        let footerLabel = UILabel()
+        footerLabel.text = "End of the list"
+        footerLabel.textColor = UIColor.systemBlue
+        footerLabel.translatesAutoresizingMaskIntoConstraints = false // Enable Auto Layout
+        footerLabel.font = UIFont.systemFont(ofSize: 12)
+        
+        // Add the label to the footer view
+        footerView.addSubview(footerLabel)
+        
+        // Center the label in the footer view
+        NSLayoutConstraint.activate([
+            footerLabel.centerXAnchor.constraint(equalTo: footerView.centerXAnchor),
+            footerLabel.centerYAnchor.constraint(equalTo: footerView.centerYAnchor)
+        ])
+        
+        return footerView
+    }
+
+    
 }
 

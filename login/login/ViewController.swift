b650d1f3ffd1245609b93d8c1ed83ca797f88682
P10 <p10@P10s-Mac-mini.local>
2023-10-16 11:44:32 +0530

    Added login module

--- login/login/ViewController.swift
+++ login/login/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func logInBtnOnSubmit(_ sender: Any) {
+                // Ensure that the username and password fields are not empty
+                guard let username = userNameTF.text, !username.isEmpty,
+                      let password = passwordTF.text, !password.isEmpty else {
+                    showAlert(title: "Validation Error", message: "Please enter both username and password.")
+                    return
+                }
 
+                // You can add additional validation here, e.g., minimum length for the password
+                if password.count < 6 {
+                    showAlert(title: "Validation Error", message: "Password must be at least 6 characters long.")
+                    return
+                }
+
+    }
+    
+    func showAlert(title: String, message: String) {
+           let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+           self.present(alert, animated: true, completion: nil)
+       }
+    
 
 }
 

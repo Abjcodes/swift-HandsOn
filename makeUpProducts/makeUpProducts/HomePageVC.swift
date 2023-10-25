0cc9776a602fd6d37c50150e8b6585bf6db75f14
P10 <p10@P10s-Mac-mini.local>
2023-10-25 17:50:35 +0530

    Added alert

--- makeUpProducts/makeUpProducts/HomePageVC.swift
+++ makeUpProducts/makeUpProducts/HomePageVC.swift
@@ -49,21 +49,32 @@ class HomePageVC: UIViewController {
            loadingView?.isHidden = true
        }
        
-       private func fetchDataFromAPI() {
-           loadingView?.isHidden = false
-           loadingView?.startLoading()
-           ProductService.shared.fetchMakeupProducts { [weak self] (fetchedProducts, error) in
-               guard let self = self else { return }
-               if let error = error {
-                   print("Error fetching data: \(error.localizedDescription)")
-               } else if let fetchedProducts = fetchedProducts {
-                   print("Fetched \(fetchedProducts.count) products")
-                   self.products = fetchedProducts
-                   DispatchQueue.main.async {
-                       self.loadingView?.stopLoading()
-                       self.tableView.reloadData()
-                   }
-               }
-           }
-       }
+    private func fetchDataFromAPI() {
+        loadingView?.isHidden = false
+        loadingView?.startLoading()
+        ProductService.shared.fetchMakeupProducts { [weak self] (fetchedProducts, error) in
+            guard let self = self else { return }
+            if let error = error {
+                DispatchQueue.main.async {
+                    // Create and present an error alert
+                    self.presentErrorAlert(message: "Error fetching data: \(error.localizedDescription)")
+                    self.loadingView?.stopLoading()
+                }
+            } else if let fetchedProducts = fetchedProducts {
+                print("Fetched \(fetchedProducts.count) products")
+                self.products = fetchedProducts
+                DispatchQueue.main.async {
+                    self.loadingView?.stopLoading()
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+
+    private func presentErrorAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
 }
